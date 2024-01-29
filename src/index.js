bf0595433fbd80cab9df1eae10dc1a0232cba50b
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2024-01-29 12:51:49 +0200

    fix:feedback

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import SlimSelect from "slim-select";
-import {fetchBreeds, fetchCatByBreed} from "./cat-api";
+import SlimSelect from 'slim-select';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const selector = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
-
+const loader = document.querySelector('.loader');
 
 handleBreeds();
 
-function handleBreeds() {
+async function handleBreeds() {
   selector.style.display = 'none';
-  fetchBreeds('breeds').then(res => {
+  loader.style.display = 'block';
+  try {
+    const response = await fetchBreeds();
     selector.style.display = 'flex';
-    initSelect(res)
-  })
+    initSelect(response);
+  } catch (error) {
+    iziToast.error({
+      timeout: 5000,
+      position: 'topRight',
+      message: 'Oops! Something went wrong! Try reloading the page!',
+    });
+  } finally {
+    loader.style.display = 'none';
+  }
 }
 
 function initSelect(arr) {
@@ -21,14 +34,13 @@ function initSelect(arr) {
     settings: {
       showSearch: false,
     },
-    data:
-      arr.map(({name, id}) => {
-        return {
-          text: name,
-          value: name,
-          id: id,
-        };
-      }),
+    data: arr.map(({ name, id }) => {
+      return {
+        text: name,
+        value: name,
+        id: id,
+      };
+    }),
     events: {
       beforeChange: () => catInfo.replaceChildren(),
       afterChange: handleSelectChange,
@@ -36,14 +48,26 @@ function initSelect(arr) {
   });
 }
 
-function handleSelectChange(result) {
-  fetchCatByBreed(result[0].id)
-    .then(res => {
-      catInfo.insertAdjacentHTML('beforeend', createCatDetailMark(res[0]))
-    })
+async function handleSelectChange(result) {
+  loader.style.display = 'block';
+  try {
+    const response = await fetchCatByBreed(result[0].id);
+
+    if (!response.length) throw new Error('Please, select another breed!');
+
+    catInfo.insertAdjacentHTML('beforeend', createCatDetailMark(response[0]));
+  } catch (error) {
+    iziToast.error({
+      timeout: 10000,
+      position: 'topRight',
+      message: error.message,
+    });
+  } finally {
+    loader.style.display = 'none';
+  }
 }
 
-function createCatDetailMark({url, breeds}) {
+function createCatDetailMark({ url, breeds }) {
   return `
     <img class="cat-info__image" src="${url}" >
     <div class="cat-info__info">
